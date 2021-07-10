98f674d292abcf0b4a82f21460878c13533a861b
erikbackman <erikbackman@users.noreply.github.com>
2021-07-11 00:48:35 +0200

    add my cachix

--- system/default.nix
+++ system/default.nix
@@ -41,6 +41,7 @@
     ispell
     ncpamixer
     pass
+    cachix
   ];
 
   programs.steam.enable = true;
@@ -141,11 +142,13 @@
     binaryCaches = [
       "https://nix-community.cachix.org/"
       "https://hydra.iohk.io" # Haskell.nix
+      "ebn.cachix.org"
     ];
 
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" # Haskell.nix
+      "ebn.cachix.org-1:Z8QlCh2aXXURAyMpMgDh7VaHo/iTJ7P8r52fxpjJ5JA="
     ];
 
     package = pkgs.nixFlakes;
