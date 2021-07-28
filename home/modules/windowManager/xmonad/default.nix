bb5070fae3676f9ca8cd39529182644a399f30a1
erikbackman <erikbackman@users.noreply.github.com>
2021-07-28 18:41:24 +0200

    xmonad: cleanup

--- home/modules/windowManager/xmonad/default.nix
+++ home/modules/windowManager/xmonad/default.nix
@@ -35,7 +35,6 @@ in {
       windowManager.xmonad.enable = true;
       initExtra = ''
           xrandr --output DP-0 --mode 3440x1440 --rate 99.98
-          ~/.fehbg &
           xset r rate 500 33
           systemctl --user restart polybar
 
