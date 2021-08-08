ed3124b2339972e876b64258d667cc42ec07c700
erikbackman <erikbackman@users.noreply.github.com>
2021-08-08 02:19:17 +0200

    update xmonad config

--- packages/ebn-xmonad/xmonad.hs
+++ packages/ebn-xmonad/xmonad.hs
@@ -1,15 +1,18 @@
-{-# OPTIONS_GHC -Wno-overflowed-literals #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-overflowed-literals #-}
 
+import qualified Codec.Binary.UTF8.String as UTF8
 import qualified Control.Exception as E
+import qualified DBus as D
+import qualified DBus.Client as D
 import qualified Data.Map as M
 import Graphics.X11.ExtraTypes
-  ( xF86XK_AudioLowerVolume
-  , xF86XK_AudioMute
-  , xF86XK_AudioNext
-  , xF86XK_AudioPause
-  , xF86XK_AudioPrev
-  , xF86XK_AudioStop
+  ( xF86XK_AudioLowerVolume,
+    xF86XK_AudioMute,
+    xF86XK_AudioNext,
+    xF86XK_AudioPause,
+    xF86XK_AudioPrev,
+    xF86XK_AudioStop,
   )
 import Graphics.X11.ExtraTypes.XF86 (xF86XK_AudioRaiseVolume)
 import System.Exit (exitSuccess)
@@ -26,24 +29,24 @@ import XMonad.Actions.WithAll (killAll)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops (ewmh, fullscreenEventHook)
 import XMonad.Hooks.InsertPosition
-  ( Focus(Newer)
-  , Position(Below)
-  , insertPosition
+  ( Focus (Newer),
+    Position (Below),
+    insertPosition,
   )
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-  ( (-?>)
-  , composeOne
-  , doCenterFloat
-  , doFullFloat
-  , isDialog
-  , isFullscreen
-  , isInProperty
+  ( composeOne,
+    doCenterFloat,
+    doFullFloat,
+    isDialog,
+    isFullscreen,
+    isInProperty,
+    (-?>),
   )
 import XMonad.Layout.BinarySpacePartition
 import XMonad.Layout.Gaps
-import XMonad.Layout.MultiToggle (Toggle(..), mkToggle, single)
-import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL))
+import XMonad.Layout.MultiToggle (Toggle (..), mkToggle, single)
+import XMonad.Layout.MultiToggle.Instances (StdTransformers (NBFULL))
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.PerWorkspace (onWorkspace)
 import XMonad.Layout.SimpleFloat (simpleFloat)
@@ -55,70 +58,69 @@ import XMonad.Util.NamedActions
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
-import qualified DBus as D
-import qualified DBus.Client as D
-import qualified Codec.Binary.UTF8.String as UTF8
 
 main :: IO ()
 main = xmonad . dynamicProjects projects . keybindings . ewmh . docks . config =<< mkDbusClient
   where
     config dbus =
-      def { manageHook = myManageHook
-          , logHook = dynamicLogWithPP (polybarHook dbus)
-          , startupHook = myStartupHook
-          , terminal = myTerminal
-          , modMask = mod4Mask
-          , borderWidth = 1
-          , handleEventHook = handleEventHook def <+> fullscreenEventHook
-          , layoutHook = myLayouts
-          , focusedBorderColor = "#bd93f9"
-          , normalBorderColor = "#434C5E"
-          , workspaces = myWS
-          }
+      def
+        { manageHook = myManageHook,
+          logHook = dynamicLogWithPP (polybarHook dbus),
+          startupHook = myStartupHook,
+          terminal = myTerminal,
+          modMask = mod4Mask,
+          borderWidth = 1,
+          handleEventHook = handleEventHook def <+> fullscreenEventHook,
+          layoutHook = myLayouts,
+          focusedBorderColor = "#bd93f9",
+          normalBorderColor = "#434C5E",
+          workspaces = myWS
+        }
 
     keybindings = addDescrKeys' ((mod4Mask, xK_F1), showKeybindings) myKeys
 
 mkDbusClient :: IO D.Client
 mkDbusClient = D.connectSession >>= \dbus -> requestBus dbus >> pure dbus
- where
-  requestBus dbus = D.requestName dbus ( D.busName_ "org.xmonad.log" ) opts
-  opts = [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+  where
+    requestBus dbus = D.requestName dbus (D.busName_ "org.xmonad.log") opts
+    opts = [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
 
 -- Emit a DBus signal on log updates
 dbusOutput :: D.Client -> String -> IO ()
 dbusOutput dbus str =
-  let opath  = D.objectPath_ "/org/xmonad/Log"
-      iname  = D.interfaceName_ "org.xmonad.Log"
-      mname  = D.memberName_ "Update"
+  let opath = D.objectPath_ "/org/xmonad/Log"
+      iname = D.interfaceName_ "org.xmonad.Log"
+      mname = D.memberName_ "Update"
       signal = D.signal opath iname mname
-      body   = [D.toVariant $ UTF8.decodeString str]
-  in  D.emit dbus $ signal { D.signalBody = body }
+      body = [D.toVariant $ UTF8.decodeString str]
+   in D.emit dbus $ signal {D.signalBody = body}
 
 polybarHook :: D.Client -> PP
-polybarHook dbus = namedScratchpadFilterOutWorkspacePP $
-  def
-  { ppOutput = dbusOutput dbus
-  , ppCurrent = wrapper focusedFg
-  , ppVisible = wrapper fg
-  , ppUrgent = wrapper red
-  , ppHidden = wrapper gray
-  , ppWsSep = ""
-  , ppSep = " :: "
-  , ppTitle = const "" --shorten 40
-  }
+polybarHook dbus =
+  namedScratchpadFilterOutWorkspacePP $
+    def
+      { ppOutput = dbusOutput dbus,
+        ppCurrent = wrapper focusedFg,
+        ppVisible = wrapper fg,
+        ppUrgent = wrapper red,
+        ppHidden = wrapper gray,
+        ppWsSep = "",
+        ppSep = " :: ",
+        ppTitle = const "" --shorten 40
+      }
   where
     wrapper c s = wrap ("%{F" <> c <> "} ") " %{F-}" s
 
-    blue      = "#2E9AFE"
-    gray      = "#7F7F7F"
-    orange    = "#ea4300"
-    purple    = "#9058c7"
+    blue = "#2E9AFE"
+    gray = "#7F7F7F"
+    orange = "#ea4300"
+    purple = "#9058c7"
     -- red       = "#722222"
     focusedFg = "#bd93f9"
-    fg        = "#ebdbb2"
-    bg        = "#282828"
-    bg1       = "#3c3836"
-    red       = "#fb4934"
+    fg = "#ebdbb2"
+    bg = "#282828"
+    bg1 = "#3c3836"
+    red = "#fb4934"
 
 myManageHook :: ManageHook
 myManageHook = manageApps <+> manageSpawn <+> manageScratchpads
@@ -146,18 +148,20 @@ myManageHook = manageApps <+> manageSpawn <+> manageScratchpads
     manageApps :: ManageHook
     manageApps =
       composeOne
-        [ match [ office ] -?> doFloat
-        , match [btm, spotify, mpv, yad] -?> doFullFloat
-        , resource =? "desktop_window" -?> doIgnore
-        , resource =? "kdesktop" -?> doIgnore
-        , anyOf [ isBrowserDialog
-                , isFileChooserDialog
-                , isDialog
-                , isPopup
-                , isSplash
-                ] -?> doCenterFloat
-        , isFullscreen -?> doFullFloat
-        , pure True -?> tileBelow
+        [ match [office] -?> doFloat,
+          match [btm, spotify, mpv, yad] -?> doFullFloat,
+          resource =? "desktop_window" -?> doIgnore,
+          resource =? "kdesktop" -?> doIgnore,
+          anyOf
+            [ isBrowserDialog,
+              isFileChooserDialog,
+              isDialog,
+              isPopup,
+              isSplash
+            ]
+            -?> doCenterFloat,
+          isFullscreen -?> doFullFloat,
+          pure True -?> tileBelow
         ]
 
 isInstance :: App -> Query Bool
@@ -193,52 +197,64 @@ myWS = [webWs, devWs, comWs, wrkWs, sysWs, etcWs]
 projects :: [Project]
 projects =
   [ Project
-      { projectName = webWs
-      , projectDirectory = "~/"
-      , projectStartHook = Just $ spawn "chromium-browser"
-      }
-  , Project
-      { projectName = devWs
-      , projectDirectory = "~/source/github.com/erikbackman"
-      , projectStartHook = Just $ spawn myEditor
-      }
-  , Project
-      { projectName = comWs
-      , projectDirectory = "~/"
-      , projectStartHook =
+      { projectName = webWs,
+        projectDirectory = "~/",
+        projectStartHook = Just $ spawn "chromium-browser"
+      },
+    Project
+      { projectName = devWs,
+        projectDirectory = "~/repos/github.com/erikbackman",
+        projectStartHook = Just $ spawn myEditor
+      },
+    Project
+      { projectName = comWs,
+        projectDirectory = "~/",
+        projectStartHook =
           Just $
-          spawn mailClient >>
-          spawn "discord"
+            spawn mailClient
+              >> spawn "discord"
+      },
+    Project
+      { projectName = wrkWs,
+        projectDirectory = "~/",
+        projectStartHook = Nothing
+      },
+    Project
+      { projectName = sysWs,
+        projectDirectory = "~/repos/github.com/erikbackman/nixos-config",
+        projectStartHook = Nothing
+      },
+    Project
+      { projectName = etcWs,
+        projectDirectory = "~/",
+        projectStartHook = Nothing
       }
-  , Project
-      {projectName = wrkWs, projectDirectory = "~/", projectStartHook = Nothing}
-  , Project
-      { projectName = sysWs
-      , projectDirectory = "~/source/dotfiles"
-      , projectStartHook = Nothing
-      }
-  , Project
-      {projectName = etcWs, projectDirectory = "~/", projectStartHook = Nothing}
   ]
 
 myLayouts =
-  avoidStruts .
-  smartBorders .
-  fullScreenToggle . comLayout . devLayout . webLayout . etcLayout . wrkLayout $
-  (tiled ||| Mirror tiled ||| column3 ||| full)
-     -- default tiling algorithm partitions the screen into two panes
+  avoidStruts
+    . smartBorders
+    . fullScreenToggle
+    . comLayout
+    . devLayout
+    . webLayout
+    . etcLayout
+    . wrkLayout
+    $ (tiled ||| Mirror tiled ||| column3 ||| full)
   where
+    -- default tiling algorithm partitions the screen into two panes
+
     tiled = gapSpaced 10 $ Tall nmaster delta ratio
     full = gapSpaced 5 Full
     column3 = gapSpaced 10 $ ThreeColMid 1 (3 / 100) (1 / 2)
     float = simpleFloat
-    mirrorTall = gapSpaced 10 $ Mirror (Tall 1 (3/100) (1/2))
+    mirrorTall = gapSpaced 10 $ Mirror (Tall 1 (3 / 100) (1 / 2))
     -- The default number of windows in the master pane
     nmaster = 1
     -- Default proportion of screen occupied by master pane
     ratio = 1 / 2
     -- Percent of screen to increment by when resizing panes
-    delta = 3 / 100-- Gaps bewteen windows
+    delta = 3 / 100 -- Gaps bewteen windows
     myGaps gap = gaps [(U, gap), (D, gap), (L, gap), (R, gap)]
     gapSpaced g = spacing g . myGaps g
     -- Per workspace layout
@@ -249,8 +265,9 @@ myLayouts =
     etcLayout = onWorkspace etcWs float
     -- Fullscreen
     fullScreenToggle = mkToggle (single NBFULL)
-    -- Misc
-    -- master l = fixMastered (1 / 4) (1 / 2) l
+
+-- Misc
+-- master l = fixMastered (1 / 4) (1 / 2) l
 
 myStartupHook :: X ()
 myStartupHook = do
@@ -316,85 +333,87 @@ playerctl c = "playerctl --player=spotify,%any " <> c
 showKeybindings :: [((KeyMask, KeySym), NamedAction)] -> NamedAction
 showKeybindings x =
   addName "Show Keybindings" . io $
-  E.bracket
-    (spawnPipe $ getAppCommand yad)
-    hClose
-    (\h -> hPutStr h (unlines $ showKm x))
+    E.bracket
+      (spawnPipe $ getAppCommand yad)
+      hClose
+      (\h -> hPutStr h (unlines $ showKm x))
 
 myKeys conf@XConfig {XMonad.modMask = modm} =
-  keySet "Applications" [key "Slack" (modm, xK_F2) $ spawnOn comWs "slack"] ^++^
-  keySet
-    "Audio"
-    [ key "Mute" (0, xF86XK_AudioMute) $ spawn "amixer -q set Master toggle"
-    , key "Lower volume" (0, xF86XK_AudioLowerVolume) $
-      spawn "amixer -q set Master 5%-"
-    , key "Raise volume" (0, xF86XK_AudioRaiseVolume) $
-      spawn "amixer -q set Master 5%+"
-    , key "Play / Pause" (0, xF86XK_AudioPause) $ spawn $ playerctl "play-pause"
-    , key "Stop" (0, xF86XK_AudioStop) $ spawn $ playerctl "stop"
-    , key "Previous" (0, xF86XK_AudioPrev) $ spawn $ playerctl "previous"
-    , key "Next" (0, xF86XK_AudioNext) $ spawn $ playerctl "next"
-    ] ^++^
-  keySet
-    "Launchers"
-    [ key "Terminal" (modm .|. shiftMask, xK_Return) $ spawn myTerminal
-    , key "Apps (Rofi)" (modm, xK_p) $ spawn appLauncher
-    -- , key "Lock screen"   (modm .|. controlMask, xK_l       ) $ spawn screenLocker
-    , key "Emacs everywhere!" (modm .|. controlMask, xK_F10) $
-      spawn "emacsclient --eval \"(emacs-everywhere)\""
-    ] ^++^
-  keySet
-    "Layouts"
-    [ key "Next" (modm, xK_space) $ sendMessage NextLayout
-    , key "Reset" (modm .|. shiftMask, xK_space) $
-      setLayout (XMonad.layoutHook conf)
-    , key "Fullscreen" (modm, xK_f) $ sendMessage (Toggle NBFULL)
-    , key "Float" (modm .|. shiftMask, xK_t) $ withFocused toggleFloat
-    ] ^++^
-  keySet
-    "Scratchpads"
-    [ key "mpv" (modm .|. controlMask, xK_a) $ runScratchpadApp mpv
-    , key "bottom" (modm .|. controlMask, xK_y) $ runScratchpadApp btm
-    , key "Spotify" (modm .|. controlMask, xK_s) $ runScratchpadApp spotify
-    ] ^++^
-  keySet
-    "System"
-    [ key "Toggle status bar gap" (modm, xK_b) toggleStruts
-    , key "Logout (quit XMonad)" (modm .|. shiftMask, xK_q) $ io exitSuccess
-    , key "Restart XMonad" (modm, xK_q) $
-      spawn "xmonad --recompile; xmonad --restart"
-    , key "Capture entire screen" (modm, xK_Print) $
-      spawn "flameshot full -p ~/Pictures/flameshot/"
-    ] ^++^
-  keySet
-    "Windows"
-    [ key "Close focused" (modm, xK_BackSpace) kill
-    , key "Close all in ws" (modm .|. shiftMask, xK_BackSpace) killAll
-    , key "Refresh size" (modm, xK_n) refresh
-    , key "Focus next" (modm, xK_j) $ windows W.focusDown
-    , key "Focus previous" (modm, xK_k) $ windows W.focusUp
-    , key "Focus master" (modm, xK_m) $ windows W.focusMaster
-    , key "Swap master" (modm, xK_Return) $ windows W.swapMaster
-    , key "Swap next" (modm .|. shiftMask, xK_j) $ windows W.swapDown
-    , key "Swap previous" (modm .|. shiftMask, xK_k) $ windows W.swapUp
-    , key "Shrink master" (modm, xK_h) $ sendMessage Shrink
-    , key "Expand master" (modm, xK_l) $ sendMessage Expand
-    , key "Switch to tile" (modm, xK_t) $ withFocused (windows . W.sink)
-    , key "Rotate slaves" (modm .|. shiftMask, xK_Tab) rotSlavesUp
-    , key "Decrease size" (modm, xK_d) $
-      withFocused (keysResizeWindow (-10, -10) (1, 1))
-    , key "Increase size" (modm, xK_s) $
-      withFocused (keysResizeWindow (10, 10) (1, 1))
-    , key "Decr  abs size" (modm .|. shiftMask, xK_d) $
-      withFocused (keysAbsResizeWindow (-10, -10) (1024, 752))
-    , key "Incr  abs size" (modm .|. shiftMask, xK_s) $
-      withFocused (keysAbsResizeWindow (10, 10) (1024, 752))
-    ] ^++^
-  keySet
-    "Workspaces"
-    [ key "Remove" (modm .|. shiftMask, xK_F4) removeWorkspace
-    -- , key "Next" (modm, xK_comma)
-    ] ++ switchWsById ++ switchWsByIdF
+  keySet "Applications" [key "Slack" (modm, xK_F2) $ spawnOn comWs "slack"]
+    ^++^ keySet
+      "Audio"
+      [ key "Mute" (0, xF86XK_AudioMute) $ spawn "amixer -q set Master toggle",
+        key "Lower volume" (0, xF86XK_AudioLowerVolume) $
+          spawn "amixer -q set Master 5%-",
+        key "Raise volume" (0, xF86XK_AudioRaiseVolume) $
+          spawn "amixer -q set Master 5%+",
+        key "Play / Pause" (0, xF86XK_AudioPause) $ spawn $ playerctl "play-pause",
+        key "Stop" (0, xF86XK_AudioStop) $ spawn $ playerctl "stop",
+        key "Previous" (0, xF86XK_AudioPrev) $ spawn $ playerctl "previous",
+        key "Next" (0, xF86XK_AudioNext) $ spawn $ playerctl "next"
+      ]
+    ^++^ keySet
+      "Launchers"
+      [ key "Terminal" (modm .|. shiftMask, xK_Return) $ spawn myTerminal,
+        key "Apps (Rofi)" (modm, xK_p) $ spawn appLauncher,
+        -- , key "Lock screen"   (modm .|. controlMask, xK_l       ) $ spawn screenLocker
+        key "Emacs everywhere!" (modm .|. controlMask, xK_F10) $
+          spawn "emacsclient --eval \"(emacs-everywhere)\""
+      ]
+    ^++^ keySet
+      "Layouts"
+      [ key "Next" (modm, xK_space) $ sendMessage NextLayout,
+        key "Reset" (modm .|. shiftMask, xK_space) $
+          setLayout (XMonad.layoutHook conf),
+        key "Fullscreen" (modm, xK_f) $ sendMessage (Toggle NBFULL),
+        key "Float" (modm .|. shiftMask, xK_t) $ withFocused toggleFloat
+      ]
+    ^++^ keySet
+      "Scratchpads"
+      [ key "mpv" (modm .|. controlMask, xK_a) $ runScratchpadApp mpv,
+        key "bottom" (modm .|. controlMask, xK_y) $ runScratchpadApp btm,
+        key "Spotify" (modm .|. controlMask, xK_s) $ runScratchpadApp spotify
+      ]
+    ^++^ keySet
+      "System"
+      [ key "Toggle status bar gap" (modm, xK_b) toggleStruts,
+        key "Logout (quit XMonad)" (modm .|. shiftMask, xK_q) $ io exitSuccess,
+        key "Restart XMonad" (modm, xK_q) $
+          spawn "xmonad --recompile; xmonad --restart",
+        key "Capture entire screen" (modm, xK_Print) $
+          spawn "flameshot full -p ~/Pictures/flameshot/"
+      ]
+    ^++^ keySet
+      "Windows"
+      [ key "Close focused" (modm, xK_BackSpace) kill,
+        key "Close all in ws" (modm .|. shiftMask, xK_BackSpace) killAll,
+        key "Refresh size" (modm, xK_n) refresh,
+        key "Focus next" (modm, xK_j) $ windows W.focusDown,
+        key "Focus previous" (modm, xK_k) $ windows W.focusUp,
+        key "Focus master" (modm, xK_m) $ windows W.focusMaster,
+        key "Swap master" (modm, xK_Return) $ windows W.swapMaster,
+        key "Swap next" (modm .|. shiftMask, xK_j) $ windows W.swapDown,
+        key "Swap previous" (modm .|. shiftMask, xK_k) $ windows W.swapUp,
+        key "Shrink master" (modm, xK_h) $ sendMessage Shrink,
+        key "Expand master" (modm, xK_l) $ sendMessage Expand,
+        key "Switch to tile" (modm, xK_t) $ withFocused (windows . W.sink),
+        key "Rotate slaves" (modm .|. shiftMask, xK_Tab) rotSlavesUp,
+        key "Decrease size" (modm, xK_d) $
+          withFocused (keysResizeWindow (-10, -10) (1, 1)),
+        key "Increase size" (modm, xK_s) $
+          withFocused (keysResizeWindow (10, 10) (1, 1)),
+        key "Decr  abs size" (modm .|. shiftMask, xK_d) $
+          withFocused (keysAbsResizeWindow (-10, -10) (1024, 752)),
+        key "Incr  abs size" (modm .|. shiftMask, xK_s) $
+          withFocused (keysAbsResizeWindow (10, 10) (1024, 752))
+      ]
+    ^++^ keySet
+      "Workspaces"
+      [ key "Remove" (modm .|. shiftMask, xK_F4) removeWorkspace
+      -- , key "Next" (modm, xK_comma)
+      ]
+      ++ switchWsById
+      ++ switchWsByIdF
   where
     togglePolybar = spawn "polybar-msg cmd toggle &"
 
@@ -402,10 +421,11 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
 
     toggleFloat w =
       windows
-        (\s ->
-           if M.member w (W.floating s)
-             then W.sink w s
-             else W.float w (W.RationalRect (1 / 3) (1 / 4) (1 / 2) (4 / 5)) s)
+        ( \s ->
+            if M.member w (W.floating s)
+              then W.sink w s
+              else W.float w (W.RationalRect (1 / 3) (1 / 4) (1 / 2) (4 / 5)) s
+        )
     keySet s ks = subtitle s : ks
 
     key n k a = (k, addName n a)
@@ -415,15 +435,15 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
         then "Move to "
         else "Switch to "
 
-  -- mod-[1..9]: Switch to workspace N | mod-shift-[1..9]: Move client to workspace N
+    -- mod-[1..9]: Switch to workspace N | mod-shift-[1..9]: Move client to workspace N
     switchWsById =
       [ key (action m <> show i) (m .|. modm, k) (windows $ f i)
-      | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9],
+          (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
       ]
 
     switchWsByIdF =
       [ key (action m <> show i) (m, k) (windows $ f i)
-      | (i, k) <- zip (XMonad.workspaces conf) [xK_F1 .. xK_F6]
-      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_F1 .. xK_F6],
+          (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
       ]
