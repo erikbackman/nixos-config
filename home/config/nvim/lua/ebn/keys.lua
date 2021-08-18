002426bcde6cacae1dfb9b8233447dbb6d9c7e65
erikbackman <erikbackman@users.noreply.github.com>
2021-08-18 11:19:49 +0200

    nvim: keybinds

--- home/config/nvim/lua/ebn/keys.lua
+++ home/config/nvim/lua/ebn/keys.lua
@@ -4,31 +4,35 @@ local function keymap(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- General
-keymap('n', '<leader>qq', '<cmd>qa<CR>', nil) 
-keymap('n', '<leader>ot', '<cmd>term<CR>', nil) 
+-- Misc
+keymap('n', '<leader>qq', '<cmd>qa<CR>')
+keymap('n', '<leader>ot', '<cmd>term<CR>')
+keymap('n', 'Y', 'v$y')
+keymap('n', '<C-Space>', '80li<CR><Esc>')
 
 -- Buffer
-keymap('n', '<leader>fs', '<cmd>w<CR>', nil)
-keymap('n', '<leader>bk', '<cmd>bd<CR>', nil)
-keymap('n', '<leader>eb', '<cmd>luafile %<CR>', { silent = false }) 
-keymap('n', '<leader>bb', '<cmd>Buffers<CR>', nil)
-keymap('n', '<leader>sb', '<cmd>:BLines<CR>', nil)  
+keymap('n', '<leader>fs', '<cmd>w<CR>')
+keymap('n', '<leader>bk', '<cmd>bd<CR>')
+keymap('n', '<leader>eb', '<cmd>luafile %<CR>', { silent = false })
+keymap('n', '<leader>bb', '<cmd>Buffers<CR>')
+keymap('n', '<leader>sb', '<cmd>:BLines<CR>')
 
 -- Project
-keymap('n', '<leader>sp', '<cmd>Rg<CR>', nil)
+keymap('n', '<leader>sp', '<cmd>Rg<CR>')
 
 -- Files
-keymap('n', '<leader>fp', '<cmd>FZF /home/ebn/.config/nvim/lua/<CR>', nil)
-keymap('n', '<leader>pp', '<cmd>FZF /home/ebn/repos/github.com/erikbackman/<CR>', nil)
-keymap('n', '<leader>pf', '<cmd>GitFiles<CR>', nil)
-keymap('n', '<leader>ff', '<cmd>Files %:p:h<CR>', nil) -- Files in current file directory
-keymap('n', '<leader>fd', '<cmd>NnnPicker %<CR>', nil)
-keymap('n', '<leader>fr', '<cmd>:History<CR>', nil) -- Recent files
+keymap('n', '<leader>fp', '<cmd>FZF /home/ebn/.config/nvim/lua/<CR>')
+keymap('n', '<leader>pp', '<cmd>FZF /home/ebn/repos/github.com/erikbackman/<CR>')
+keymap('n', '<leader>pf', '<cmd>GitFiles<CR>')
+keymap('n', '<leader>ff', '<cmd>Files %:p:h<CR>') -- Files in current file directory
+keymap('n', '<leader>fd', '<cmd>NnnPicker %<CR>')
+keymap('n', '<leader>fr', '<cmd>:History<CR>') -- Recent files
 
--- History 
-keymap('n', '<leader>h', '<cmd>:History:<CR>', nil) -- Command history
-keymap('n', '<leader>hh', '<cmd>:History/<CR>', nil) -- Search history
+-- History
+keymap('n', '<leader>h', '<cmd>:History:<CR>') -- Command history
+keymap('n', '<leader>hh', '<cmd>:History/<CR>') -- Search history
 
 -- Docs
-keymap('n', '<leader>H', '<cmd>:Helptags<CR>', nil)
+keymap('n', '<leader>H', '<cmd>:Helptags<CR>')
+
+
