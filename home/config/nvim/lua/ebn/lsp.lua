849349e75bf87e6c281b90545533c52947fbfe01
erikbackman <erikbackman@users.noreply.github.com>
2021-08-19 05:49:00 +0200

    lsp: add client keymaps

--- home/config/nvim/lua/ebn/lsp.lua
+++ home/config/nvim/lua/ebn/lsp.lua
@@ -9,22 +9,25 @@ local on_attach = function(client, bufnr)
     -- Mappings
     local opts = { noremap=true, silent=true }
 
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-
-    buf_set_keymap('n', '<leader>bf', '<cmd>!ormolu % -m inplace<CR>', opts)
+ 
+    -- Client mappings
+    if client.name == "hls" then
+      buf_set_keymap('n', '<leader>bf', '<cmd>!ormolu % -m inplace<CR>', opts)
+    end
 end
 
-require('lspconfig').hls.setup({ on_attach = on_attach})
-require('lspconfig').rnix.setup({ on_attach = on_attach})
-require('lspconfig').ccls.setup({ on_attach = on_attach})
+require('lspconfig').hls.setup({ on_attach = on_attach })
+require('lspconfig').rnix.setup({ on_attach = on_attach })
+require('lspconfig').ccls.setup({ on_attach = on_attach })
 
 -- Lua LSP
 local runtime_path = vim.split(package.path, ';')
@@ -52,4 +55,3 @@ require('lspconfig').sumneko_lua.setup {
     },
   },
 }
-
