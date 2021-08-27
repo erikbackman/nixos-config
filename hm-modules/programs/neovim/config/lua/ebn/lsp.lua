2b3ab8fae3d5edd87a74e3b1c391bdd80499404b
erikbackman <erikbackman@users.noreply.github.com>
2021-08-27 22:42:29 +0200

    nvim: lsp keybinds

--- hm-modules/programs/neovim/config/lua/ebn/lsp.lua
+++ hm-modules/programs/neovim/config/lua/ebn/lsp.lua
@@ -13,12 +13,11 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
- 
+
     -- Client mappings
     if client.name == "hls" then
       buf_set_keymap('n', '<leader>bf', '<cmd>!ormolu % -m inplace<CR>', opts)
