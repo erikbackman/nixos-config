90b656dfac05b3c88140305f44da4667a3451883
erikbackman <erikbackman@users.noreply.github.com>
2021-09-05 20:46:51 +0200

    nvim: cleanup

--- modules/programs/neovim/default.nix
+++ modules/programs/neovim/default.nix
@@ -38,14 +38,5 @@ in with lib; {
     environment.systemPackages = with pkgs; [
       fzf
     ];
-
-    #home.file.".config/nvim/lua".source = ./config/lua;
-    #home.activation.copyLuaConfig = lib.hm.dag.entryAfter [ "writeBoundry" ] ''
-    #  if [[ -d $HOME/.config/nvim/lua ]]; then
-    #    rm -rf $HOME/.config/nvim/lua
-    #  fi
-
-    #  cp -rf ${./config/lua} $HOME/.config/nvim/lua
-    #'';
   };
 }
