6c5ddffe02102b0df80979a77982264278ac0ac3
erikbackman <erikbackman@users.noreply.github.com>
2021-09-28 01:59:09 +0200

    package: modus-themes

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,5 +3,6 @@ final: prev: {
     ebn-xmonad = import ../packages/ebn-xmonad final;
     spaceduck = import ../packages/spaceduck final;
     aurora-vim = import ../packages/aurora-vim final;
+    modus-theme-vim = import ../packages/modus-theme-vim final;
   };
 }
---
+++ packages/modus-theme-vim/default.nix
@@ -0,0 +1,13 @@
+pkgs:
+
+pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "modus-theme-vim";
+    version = "1.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "ishan9299";
+      repo = "modus-theme-vim";
+      rev = "6b82be8589a590405c1347f17402d845e52d776f";
+      sha256 = "1qyw5n9ig8pj5wnh2ic7ap897ix6nzg00cn6mcmw91aryzmgpi25";
+    };
+    meta.homepage = "https://github.com/ishan9299/modus-theme-vim";
+}
