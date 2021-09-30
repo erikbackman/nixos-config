4793e8932c8af9ee21efff3af777464a970a6470
erikbackman <erikbackman@users.noreply.github.com>
2021-09-30 11:13:34 +0200

    update bifrost

--- hosts/bifrost/default.nix
+++ hosts/bifrost/default.nix
@@ -3,12 +3,10 @@
   networking.hostName = "bifrost";
 
   nixpkgs.config.allowUnfree = true;
-  programs.steam.enable = true;
   programs.ebn.nvim.enable = true;
   programs.ebn.kitty.enable = true;
   windowManager.ebn.xmonad.enable = true;
 
-  services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.layout = "se";
   services.dbus = {
     enable = true;
